10bb9fbdbfc2ba54cf1d287511342ff7cc0c2c2c
wdea <kokoalzmr@gmail.com>
2025-08-25 23:35:55 +0300

    nn

--- lib/Screens/Home/homescreen.dart
+++ lib/Screens/Home/homescreen.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:winstappa/Widgets/uiwdea.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  var arrcontent = [
+    {"img": "assets/images/blal.png", "name": "bila_kjl"},
+    {"img": "assets/images/st2.png", "name": "abu_hid"},
+    {"img": "assets/images/st3.png", "name": "cr7"},
+    {"img": "assets/images/st4.png", "name": "snns_58h"},
+    {"img": "assets/images/st1.png", "name": "sjj_jsk"},
+    {"img": "assets/images/st2.png", "name": "ai_l8jsj"},
+    {"img": "assets/images/st3.png", "name": "anj_lkhg8s"},
+    {"img": "assets/images/st4.png", "name": "ajhs_,nnj"},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +31,111 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          SizedBox(
+            height: 90,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          AssetImage(arrcontent[index]["img"].toString()),
+                    ),
+                  );
+                },
+                itemCount: arrcontent.length,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      arrcontent[index]["name"].toString(),
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  );
+                },
+                itemCount: arrcontent.length,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 375,
+            color: Colors.black12,
+            child: ListTile(
+              leading: UiWdea.CustomImage(imgurl: "blal.png"),
+              title: Text('bilal'),
+              subtitle: Text(
+                "lbnan",
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: UiWdea.CustomImage(imgurl: "btn.png"),
+            ),
+          ),
+          Container(
+            clipBehavior: Clip.antiAlias,
+            height: 350,
+            width: double.infinity,
+            decoration: BoxDecoration(),
+            child: UiWdea.CustomImage(imgurl: "post.png"),
+          ),
+          SizedBox(
+            height: 1,
+          ),
           Row(
             children: [
-              CircleAvatar(
-                radius: 50,
-                
-              )
+              UiWdea.CustomImage(imgurl: "like.png"),
+              SizedBox(
+                width: 20,
+              ),
+              UiWdea.CustomImage(imgurl: "communt.png"),
+              SizedBox(
+                width: 20,
+              ),
+              UiWdea.CustomImage(imgurl: "reto.png"),
+              SizedBox(
+                width: 20,
+              ),
+              UiWdea.CustomImage(imgurl: "share.png"),
+              SizedBox(
+                width: 200,
+              ),
+              UiWdea.CustomImage(imgurl: "save.png"),
             ],
-          )
+          ),
+          Row(
+            children: [
+              Text("10k"),
+              SizedBox(
+                width: 25,
+              ),
+              Text("2k"),
+              SizedBox(
+                width: 25,
+              ),
+              Text("5k"),
+              SizedBox(
+                width: 25,
+              ),
+              Text("3k"),
+              SizedBox(
+                width: 200,
+              ),
+            ],
+          ),
+          Text("champion lege and ajsjsjdjd")
         ],
       ),
     );
